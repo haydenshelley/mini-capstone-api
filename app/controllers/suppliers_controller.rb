57cff88fa214232a9bd4d21181098a0c55a2806b
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-08-07 10:49:19 -0700

    create supplier, show supplier

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -5,4 +5,22 @@ class SuppliersController < ApplicationController
     render :index
   end
 
+  def create
+    @supplier = Supplier.new(
+      name: params[:name],
+      email: params[:email],
+      phone_number: params[:phone_number]
+    )
+    if @supplier.save
+      render :show
+    else
+      render json: {error: @product.errors.full_messages, status: 422}
+    end
+  end
+
+  def show
+    @supplier = Supplier.find_by(id: params[:id])
+    render :show
+  end
+
 end
