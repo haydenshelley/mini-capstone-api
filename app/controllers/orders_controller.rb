2e9c8e4821f390915b5a8f0a7d11c01c0667ece8
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-08-09 12:27:22 -0700

    remove if current user from create and show orders methods

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,28 +11,24 @@ class OrdersController < ApplicationController
     calculated_tax = product.tax * params[:quantity].to_i
     calculated_total = calculated_subtotal + calculated_tax
 
-    if current_user
-      @order = Order.new(
-        user_id: current_user.id,
-        product_id: params[:product_id],
-        quantity: params[:quantity],
-        subtotal: calculated_subtotal,
-        tax: calculated_tax,
-        total: calculated_total
-      )
-      @order.save
-      render :show
-    else
-      render json: {message: "User must be logged in to create an order"}
-    end
+    @order = Order.new(
+      user_id: current_user.id,
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total
+    )
+    @order.save
+    render :show
   end
 
   def show
-    if current_user
-      @order = current_user.orders.find_by(id: params[:id])
+    @order = Order.find_by(id: params[:id])
+    if @order.user_id == current_user.id
       render :show
     else
-      render json: {message: "User must be logged in to see order"}
+      render json: {}, status: :unauthorized
     end
   end
 
