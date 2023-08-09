3697b68c5b072278ec12a106ac0d62d627f941fb
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-08-09 10:21:47 -0700

    user must be logged in to create an order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,25 @@
 class OrdersController < ApplicationController
+
+  def index
+    @orders = Order.all 
+    render :index
+  end
+
   def create
-    @order = Order.new(
-      user_id: params[:user_id],
-      product_id: params[:product_id],
-      quantity: params[:quantity],
-      subtotal: params[:subtotal],
-      tax: params[:tax],
-      total: params[:total]
-    )
-    @order.save
-    render :show
+    if current_user
+      @order = Order.new(
+        user_id: current_user.id,
+        product_id: params[:product_id],
+        quantity: params[:quantity],
+        subtotal: params[:subtotal],
+        tax: params[:tax],
+        total: params[:total]
+      )
+      @order.save
+      render :show
+    else
+      render json: {message: "User must be logged in to create an order"}
+    end
   end
 
   def show
