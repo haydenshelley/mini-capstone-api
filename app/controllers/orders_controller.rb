c510f04813efbc285b7597d929c6ef4ab660376c
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-08-09 12:01:57 -0700

    delete commented out code from orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -41,19 +41,3 @@ class OrdersController < ApplicationController
   end
 
 end
-
-
-# if current_user
-#   @order = Order.new(
-#     user_id: current_user.id,
-#     product_id: params[:product_id],
-#     quantity: params[:quantity],
-#     subtotal: Product.find_by(id: params[:product_id]).price * params[:quantity].to_i,
-#     tax: Product.find_by(id: params[:product_id]).tax,
-#     total: Product.find_by(id: params[:product_id]).price * params[:quantity].to_i + Product.find_by(id: params[:product_id]).tax
-#   )
-#   @order.save
-#   render :show
-# else
-#   render json: {message: "User must be logged in to create an order"}
-# end
