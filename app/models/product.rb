8c03a5097a9277d68936f188441063c5a5836d46
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-08-14 10:42:42 -0700

    correct product,order, user, and carted product associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,12 @@
 class Product < ApplicationRecord
   has_many :images
   belongs_to :supplier
-  has_many :orders
+  # has_many :orders
   has_many :category_products
   has_many :categories, through: :category_products
+  has_many :carted_products
+  has_many :orders, through: :carted_products
+  has_many :users, through: :carted_products
 
   validates :name, presence: true
   validates :name, uniqueness: true
